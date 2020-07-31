997c7fbfec25103ee155311e74cda4c177bbf5c3
Samarna <63645732+Samarna@users.noreply.github.com>
2020-07-31 17:00:18 +0530

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -10,17 +10,16 @@ class Slingshot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-
+        fly(){
+        this.sling.bodyA = null;
+    }
     display(){
-        if(this.sling.bodyA != null){
+        if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
         
-    }
-    fly(){
-        this.sling.bodyA = null;
     }
 }
